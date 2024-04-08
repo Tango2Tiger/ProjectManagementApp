9becc94876afb465748bd459c0a2b23121e10d66
Jonas Leth Grønbeck <j.leth.groenbeck@gmail.com>
2024-04-08 15:04:27 +0200

    Tiløjet Steps - EmployeeSteps

--- javaFX_project/features/register_employee.feature
+++ javaFX_project/features/register_employee.feature
@@ -1,13 +1,13 @@
 Feature: An employee is registered
 
 
-Scenario: Sucesfully register new employee
-  Given the user is logged in
-  When the user registers an employee
-  Then a new employee is created
+Scenario: Successfully register new employee
+  Given there is an employee with name{string}
+  #When the user registers an employee
+  #Then a new employee is created
 
 Scenario: Register an employee that is already registered
-  Given an employee is registered in the system
-  And the user is logged in
-  When the user registers the employee again
-  Then the error message “Employee is already registered” is given
+  #Given an employee is registered in the system
+  #And the user is logged in
+  #When the user registers the employee again
+  #Then the error message “Employee is already registered” is given
---
+++ javaFX_project/src/test/java/example/cucumber/EmployeeSteps.java
@@ -0,0 +1,34 @@
+package example.cucumber;
+import dtu.projectmanagement.app.Employee;
+import dtu.projectmanagement.app.ProjectManagementApp;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.Assert.*;
+
+
+public class EmployeeSteps {
+    private Employee employee;
+    private ProjectManagementApp projectManagementApp;
+    public EmployeeSteps(ProjectManagementApp projectManagementApp) {
+        this.projectManagementApp = projectManagementApp;
+
+    }
+    @Given("there is an employee with name\\{string}")
+    public void there_is_an_employee_with_name() {
+        employee = new Employee("Hubert", "Baumeister");
+    }
+    //@When("the user registers an employee")
+    //public void the_user_registers_an_employee() {
+      //  projectManagementApp.getEmployeeList();
+
+    }
+    @Then("a new employeee is created")
+    public void aNewEmployeeIsCreated() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+}
+
