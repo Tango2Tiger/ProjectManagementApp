79bf4a8da5796c7786906e982792152003ad2892
s235223 <s235223@dtu.dk>
2024-04-10 16:30:50 +0200

    Ui opdateret igen

--- javaFX_project/src/main/java/dtu/example/ui/registerEmployeeController.java
+++ javaFX_project/src/main/java/dtu/example/ui/registerEmployeeController.java
@@ -1,5 +1,20 @@
 package dtu.example.ui;
 
-public class registerEmployeeController {
+import dtu.projectmanagement.app.OperationNotAllowedException;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
+public class registerEmployeeController {
+    public TextField firstNameTxt;
+    public TextField lastNameTxt;
+    public Label registerFailTxt;
+    public void registerEmployee(ActionEvent actionEvent){
+        try{
+            App.getProjectManagementApp().registerEmployee(firstNameTxt.getText(), lastNameTxt.getText());
+            registerFailTxt.setText("");
+        } catch (OperationNotAllowedException e) {
+            registerFailTxt.setText(e.getMessage());
+        }
+    }
 }
