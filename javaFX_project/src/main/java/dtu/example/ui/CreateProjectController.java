b4e2863cb561aec2c01b09bfcbda915179f41bb5
Clement Koefoed <clementkrasmussen@gmail.com>
2024-04-15 16:21:53 +0200

    UI til create_project scenario 1

--- javaFX_project/src/main/java/dtu/example/ui/CreateProjectController.java
+++ javaFX_project/src/main/java/dtu/example/ui/CreateProjectController.java
@@ -2,9 +2,12 @@ package dtu.example.ui;
 
 import dtu.projectmanagement.app.OperationNotAllowedException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 import java.io.IOException;
 
@@ -19,15 +22,28 @@ public class CreateProjectController {
     }
 
     public void createProject(ActionEvent actionEvent) throws IOException {
-        try{
+        try {
             App.getProjectManagementApp().createProject(projectname.getText());
             projectAlreadyExistsFail.setText(projectname + " project has been added to the system!");
             App.setRoot("loggedIn");
-        }
-        catch(OperationNotAllowedException e){
+        } catch (OperationNotAllowedException e) {
             projectAlreadyExistsFail.setText(e.getMessage());
         }
 
+    }
+
+    @FXML
+    private void keyListenerCreateProject(KeyEvent event) throws IOException {
+        if (event.getCode() == KeyCode.ENTER) {
+            try {
+                App.getProjectManagementApp().createProject(projectname.getText());
+                projectAlreadyExistsFail.setText(projectname + " project has been added to the system!");
+                App.setRoot("loggedIn");
+            } catch (OperationNotAllowedException e) {
+                projectAlreadyExistsFail.setText(e.getMessage());
+            }
+
+        }
     }
 
 }
