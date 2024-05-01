0ea3f5f140825196e5ddb4e0ee0872378d702b50
Heemag <magnus.henneberg@gmail.com>
2024-05-01 17:01:18 +0200

    feature update. alt fungerer.

--- javaFX_project/features/create_activity.feature
+++ javaFX_project/features/create_activity.feature
@@ -3,5 +3,12 @@ Feature: Employee creates a activity.
     Scenario: Employee creates an activity succesfully
         Given there exists a project with the name "p1"
         And the project "p1" has no activity with the name "a1"
-        When the employee creates an activity with the name "a1"
-        #Then there is an activity with the name "a1"
+        When the employee creates an activity with the name "a1" under the project "p1"
+        Then there is an activity with the name "a1" under the project "p1"
+
+
+    Scenario: Employee tries to create activity that already exists
+        #Given there exists a project with the name "p1"
+        #And the project "p1" has no activity with the name "a1"
+        #When the employee creates an activity with the name "a1" under the project "p1"
+        #Then there is an activity with the name "a1" under the project "p1"
--- javaFX_project/src/test/java/example/cucumber/ActivitySteps.java
+++ javaFX_project/src/test/java/example/cucumber/ActivitySteps.java
@@ -1,15 +1,39 @@
 package example.cucumber;
 
-import dtu.projectmanagement.app.Employee;
-import dtu.projectmanagement.app.ProjectManagementApp;
+
+import dtu.projectmanagement.app.*;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.Assert.*;
 
 public class ActivitySteps {
+    private ProjectHolder projectHolder;
     private Employee employee;
+    private Project project;
     private ProjectManagementApp projectManagementApp;
     private ErrorMessageHolder errorMessageHolder;
-    public ActivitySteps(ProjectManagementApp projectManagementApp, ErrorMessageHolder errorMessageHolder) {
+    public ActivitySteps(ProjectManagementApp projectManagementApp, ErrorMessageHolder errorMessageHolder, ProjectHolder projectHolder) {
         this.projectManagementApp = projectManagementApp;
         this.errorMessageHolder = errorMessageHolder;
+        this.projectHolder = projectHolder;
     }
 
+    @When("the employee creates an activity with the name {string} under the project {string}")
+    public void the_employee_creates_an_activity_with_the_name(String name, String p1) {
+        project = new Project(p1);
+        projectHolder.setProject(project);
+        try {
+            projectManagementApp.createActivity(project, name);
+        } catch (OperationNotAllowedException e) {
+            errorMessageHolder.setErrorMessage(e.getMessage());
+        }
+    }
+
+
+    @Then("there is an activity with the name {string} under the project {string}")
+    public void thereIsAnActivityWithTheName(String a1, String p1) {
+        assertTrue(project.hasActivityWithName(a1));
+    }
 }
