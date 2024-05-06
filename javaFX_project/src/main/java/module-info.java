a7ac7883a22c3139de62115aaf37b5ca75be4300
Jonas Leth Grønbeck <j.leth.groenbeck@gmail.com>
2024-05-06 19:41:08 +0200

    Build Maven tests correct

--- javaFX_project/src/main/java/module-info.java
+++ javaFX_project/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module hellofx {
     requires javafx.controls;
     requires javafx.fxml;
-
-    opens dtu.projectmanagement.ui to javafx.fxml; // Gives access to fxml files
-    exports dtu.projectmanagement.ui; // Exports the class inheriting from javafx.application.Application
+    exports dtu.projectmanagement.businesslogic;
+    opens dtu.projectmanagement.ui to javafx.fxml; // Gives access to fxml files'
 }
