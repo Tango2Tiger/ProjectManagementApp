5e584f414ebfa7eac65afed7b2cd1931fcf81c33
Clement Koefoed <clementkrasmussen@gmail.com>
2024-05-06 19:53:09 +0200

    tilføjet exports

--- javaFX_project/src/main/java/module-info.java
+++ javaFX_project/src/main/java/module-info.java
@@ -2,5 +2,6 @@ module hellofx {
     requires javafx.controls;
     requires javafx.fxml;
     exports dtu.projectmanagement.businesslogic;
+    exports dtu.projectmanagement.ui;
     opens dtu.projectmanagement.ui to javafx.fxml; // Gives access to fxml files'
 }
